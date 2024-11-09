fe725997bbd992ccc735912b12d5c757b7bb7408
Vladimir Kimbris <kimbris.vladimir@wb.ru>
2024-11-10 00:29:25 +0300

    iluhas model works

--- deployment/backend/equipment/model.py
+++ deployment/backend/equipment/model.py
@@ -4,7 +4,7 @@ from flair.data import Sentence
 from flair.models import TextClassifier
 
 
-model = TextClassifier.load('equipment-model.pt')
+model = TextClassifier.load('final-model.pt')
 
 
 class PredictionInfo:
