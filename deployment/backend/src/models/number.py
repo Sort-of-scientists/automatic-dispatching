280736a4315dc1063be7494d73d62965115c7c81
itisiriska <lana.studyingbox@yandex.ru>
2024-11-09 14:09:57 +0300

    added regex model for serial numbers detection

--- deployment/backend/src/models/number.py
+++ deployment/backend/src/models/number.py
@@ -1,10 +1,13 @@
+import re
+
 from typing import List
+
 
 class NumberModel:
     """
     Модель для определения серийного номера.
     """
-    
+
     def __init__(self, path: str) -> None:
         """
         Инициалиирует модель.
@@ -14,7 +17,7 @@ class NumberModel:
         path : str
             Путь до папки/файла с моделью.
         """
-        
+
         pass
 
     def predict(self, texts: List[str]) -> List[str]:
@@ -31,3 +34,14 @@ class NumberModel:
 
         """
         return ["string"]
+
+
+class RegexNumberModel(NumberModel):
+
+    def __init__(self, path: str) -> None:
+        self.serial_number_regex = re.compile(r'([A-ZА-Я]{1,3}\d{7,15})')
+
+    def predict(self, texts: List[str]) -> List[str]:
+        predicts = [re.findall(self.serial_number_regex, text.upper()) for text in texts]
+        predicts = [p[0] if len(p) > 0 else None for p in predicts]
+        return predicts
