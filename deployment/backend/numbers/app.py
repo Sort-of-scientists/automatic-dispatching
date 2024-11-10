600c7d79d2e0231f660ff3f0b7f7ce6d958efc7b
itisiriska <lana.studyingbox@yandex.ru>
2024-11-10 07:55:18 +0300

    fixed mapping issue

--- deployment/backend/numbers/app.py
+++ deployment/backend/numbers/app.py
@@ -4,7 +4,6 @@ from fastapi import FastAPI
 from typing import List
 
 from pydantic import BaseModel
-from unidecode import unidecode
 
 
 class Prediction(BaseModel):
@@ -16,11 +15,38 @@ class RegexNumberModel:
 
     def __init__(self) -> None:
         self.serial_number_regex = re.compile(r'([A-ZА-Я]{1,3}\d{7,15})')
+        self.mapper = {
+            'А': 'A',
+            'В': 'B',
+            'Е': 'E',
+            'З': 'Z',
+            'К': 'K',
+            'М': 'M',
+            'Н': 'H',
+            'О': 'O',
+            'Р': 'P',
+            'С': 'C',
+            'Т': 'T',
+            'У': 'Y',
+            'Х': 'X'
+        }
 
     def predict(self, texts: List[str]) -> List[str]:
-        predicts = [re.findall(self.serial_number_regex, text.upper()) for text in texts]
-        predicts = [unidecode(p[0]) if len(p) > 0 else 'Уточнить' for p in predicts]
-        return predicts
+        predicts = [re.findall(self.serial_number_regex, text.upper()) for text in texts]       
+        upd_preds = []
+
+        for p in predicts:
+            p = p[0] if len(p) > 0 else 'Уточнить'
+            if p != 'Уточнить':
+                p = self._replace(p)
+            upd_preds.append(p)
+
+        return upd_preds
+
+    def _replace(self, predict):
+        for key in self.mapper.keys():
+            predict = predict.replace(key, self.mapper[key])
+        return predict
 
 
 app = FastAPI()
